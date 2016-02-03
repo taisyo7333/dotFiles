c422cb202d618ff3055f950bd324cceafa57eab3
Daisuke Inoue <d-inoue@localhost.localdomain>
2016-02-03 19:29:05 +0900

    Untracked auto-installed files.

--- .emacs.d/elpa/inf-ruby-20151104.1237/inf-ruby-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "inf-ruby" "20151104.1237" "Run a Ruby process in a buffer" (quote nil))
--- .emacs.d/elpa/inflections-20121016.157/inflections-autoloads.el
+++
@@ -1,34 +0,0 @@
-;;; inflections-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (pluralize-string singularize-string) "inflections"
-;;;;;;  "inflections.el" (22191 15412 340000 0))
-;;; Generated autoloads from inflections.el
-
-(autoload 'singularize-string "inflections" "\
-
-
-\(fn STR)" nil nil)
-
-(autoload 'pluralize-string "inflections" "\
-
-
-\(fn STR)" nil nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("inflections-pkg.el") (22191 15412 375340
-;;;;;;  46000))
-
-;;;***
-
-(provide 'inflections-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; inflections-autoloads.el ends here
