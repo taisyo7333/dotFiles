b80f4afa5cf1a6d30140308aa057be9c27090d63
Daisuke Inoue <d-inoue@localhost.localdomain>
2016-02-03 19:20:13 +0900

    Add Robe + auto-complete

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,34 +7,52 @@
   (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
       (normal-top-level-add-subdirs-to-load-path)))
 
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(package-initialize)
+(when (require 'package nil t) 
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+  (add-to-list 'package-archives '("ELPA" . "http://tromey.com/elpa/"))
+  ;; インストールするディレクトリを指定
+  (setq package-user-dir(concat user-emacs-directory "elpa" ))
+  ;; インスト−ルしたパッケージにロードパスを通して読み込む
+  (package-initialize)
+)
 
 (require 'cl)
 
 (defvar installing-package-list
   '(
     ;; ここに使っているパッケージを書く。
-    init-loader
-    markdown-mode
-    scss-mode
-    haskell-mode
-    google-c-style
-    yaml-mode
-    open-junk-file
+    ;; Alphabet order asc
+    anything
+    ;; anything-complete ; is not available
+    ;; anything-config
+    ;;anything-match-plugin
+
     auto-complete
-    ruby-mode
+    buttercup
     enh-ruby-mode
-    ruby-electric
-    rspec-mode
-    mode-compile
-    rinari
-;;    rthml-Mode
-    ruby-block
     flycheck
+    ;; google-c-style
+    ;; haskell-mode
 ;;    hlinum
+    init-loader
+    inf-ruby
+    mode-compile
+    markdown-mode
+    ;; open-junk-file 
+;;    rthml-Mode
+    rinari
+    rspec-mode
+    robe
+    ruby-compilation
+    ruby-block    
+    ruby-mode
+    ruby-electric
+    scss-mode
+
+    web-mode
+    yaml-mode
     ))
 
 (let ((not-installed (loop for x in installing-package-list
@@ -45,127 +63,56 @@
     (dolist (pkg not-installed)
         (package-install pkg))))
 
-
-
 (require 'init-loader)
 (setq init-loader-show-log-after-init nil)
 (init-loader-load "~/.emacs.d/inits")
 
-
 ;;;;;;;;;;;;;;;;;;
+;; http://codeout.hatenablog.com/entry/2014/02/04/210237
+;; Robeを起動させたいバッファで下記の作業を行う。
+;; M-x inf-ruby ( 初回のみ )
+;; M-x robe-mode
+;; M-x robe-start
+
+(autoload 'ruby-mode "ruby-mode" "Mode for editing ruby source files" t)
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
+(add-hook 'ruby-mode-hook '(lambda ()
+                             (require 'rcodetools)
+                             (require 'anything-rcodetools)
+                             (require 'myrurema)
+                             (load-auto-complete)  ;; 追加
+                             (define-key ruby-mode-map "\M-c" 'rct-complete-symbol)
+                             (define-key ruby-mode-map "\M-d" 'xmp)
+                             (setq ruby-deep-indent-paren-style nil)))
+
+; auto-complete
+(defun load-auto-complete ()
+  (require 'auto-complete-config)
+  (ac-config-default)
+
+  (add-to-list 'ac-dictionary-directories "~/.emacs.d/etc/auto-complete")
+
+  (setq ac-use-menu-map t)
+  (define-key ac-menu-map "\C-n" 'ac-next)
+  (define-key ac-menu-map "\C-p" 'ac-previous)
+
+  (setq ac-auto-show-menu 0.5)
+  (setq ac-menu-height 20)
+
+  (robe-mode))
+
+; robe
+(autoload 'robe-mode "robe" "Code navigation, documentation lookup and completion for Ruby" t nil)
+(autoload 'ac-robe-setup "ac-robe" "auto-complete robe" nil nil)
+(add-hook 'robe-mode-hook 'ac-robe-setup)
 
 
-;~/lib/emacs/.el
-;にユーザー用の[*.el , *.elc ]を置くことができる
-;.emacs.my.el 
-;を入れる。
-
-;;C言語のインデント量を「４」にする
-( setq c-mode-hook
-          '( lambda ()
-	       ( c-set-style "stroustrup" )
-	         ( setq c-basic-offset 4 )
-		   (cpp-highlight-buffer t)       ;;#if 0- #endif ﾊｲﾗｲﾄ
-		     ))
-;(setq tab-width 4);タブ幅４
-;(setq c-argdecl-indent 4)
-;(setq c-indent-level 4 ) ;デフォルトは２
-;(setq c-continued-statement-offset 4) ;デフォルトは２
-;(setq c-tab-width 4 ) 
-(setq c-tab-always-indent t) ;半自動インデント
-(setq lisp-indent-line 4 );
-;バックアップファイルを作らない
-(setq backup-inhibited t)
-;;#if 0 - #endif の中身をハイライト
-(setq cpp-highlight-buffer t)
-;; C++ style
-(add-hook 'c++-mode-hook
-          '(lambda()
-             (c-set-style "stroustrup")
-             (c-set-offset 'innamespace 0)   ; namespace {}の中はインデントしない
-             )) 
-
-;--------------------------------------------------
-;; cpp-highlight-bufferの設定
-;--------------------------------------------------
-;; knownなやつも表示
-(setq cpp-known-face 'default)
-;; unknownなやつはハイライトする
-(setq cpp-unknown-face 'highlight)
-;; 選ぶのはlight background
-(setq cpp-face-type 'light)
-;; knownもunknownもwritable
-(setq cpp-known-writable 't)
-(setq cpp-unknown-writable 't)
-;;  symbol              true            false           writable
-;;  -----------------------------------------------------------------
-;;  0                   Light gray      default         both
-;;  1                   default         light gray      both
-(setq cpp-edit-list
-      '((#("1" 0 1
-	      (fontified t c-in-sws t))
-	  nil
-	   (background-color . "medium purple")
-	    both nil)
-	(#("0" 0 1
-	      (fontified t c-in-sws t))
-	  (background-color . "medium purple")
-	   nil both nil)))
-
-;;(if (string-match "XEmacs" emacs-version)
-;;    (+ 2 3);dammy
-;;    (load (expand-file-name "~/.emacs.d/key_bind.el") nil t nil)
-;;)
-
-;; Ruby - setting
-
-;; error
-;; (require 'rinari)			;
-(add-hook 'rhtml-mode-hook
-	  (lambda () (rinari-launch)))
-
-;; ruby-block を導入すると, end に対応する行をハイライトしてくれる.
-(require 'ruby-block)
-(ruby-block-mode t)
-(setq ruby-block-highlight-toggle t)
-
-;; ruby-electric はかっこや do end などの対応関係を自動で補正してくれる.
-(require 'ruby-electric)
-(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
-(setq ruby-electric-expand-delimiters-list nil)
-
-(require 'enh-ruby-mode)
-;(autoload 'ruby-mode "ruby-mode"
-;  "Mode for editing ruby source files." t )
-(add-to-list 'auto-mode-alist '("\\.rb$latex " . ruby-mode))
-(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
-
-(require 'flycheck)
-(setq flycheck-check-syntax-automatically '(mode-enabled save))
-(add-hook 'ruby-mode-hook 'flycheck-mode)
-
-;; 現在、どこの関数内にいるかを常に画面上部に表示する
-(which-func-mode t)
-;; 全てのメジャーモードに対して which-func-modeを適用する。
-(setq which-func-modes t)
-
-;; 画面上部に表示する場合は、下２桁が必要。
-(delete (assoc 'which-func-mode mode-line-format) mode-line-format)
-(setq-default header-line-format '(which-func-mode ("" which-func-format))) 
-
-;; 現在の行をハイライトする。
-;;(require 'hlinum)
-;;(hlinum-activate)
-;(global-linum-mode t)
 
 ;; このファイルに間違いがあった場合に全てを無効にします
 (put 'eval-expression 'disabled nil)
 
-;
-;describe-key ;キーに割り当てられている関数を知ることができる
-;describe-function;関数を調べたいときに使う
-;apropos 検索キーを含む変数、関数名を列挙する
+
 
 
 
