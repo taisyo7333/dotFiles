b99551c83c52b03b769833fa404ae5f90a763c6a
Daisuke <taisyo9170@yahoo.co.jp>
2016-06-02 14:10:42 +0900

    Shell setting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -377,3 +377,60 @@ Intended for use in PROJECTILE-AFTER-SWITCH-PROJECT-HOOK."
    (setq exec-path (append (list dir) exec-path))))
 
 (setenv "MANPATH" (concat "/usr/local/man:/usr/share/man:/Developer/usr/share/man:/sw/share/man" (getenv "MANPATH")))
+
+;; shell の存在を確認
+(defun skt:shell ()
+  (or (executable-find "bash")
+      (executable-find "zsh")
+      ;; (executable-find "f_zsh") ;; Emacs + Cygwin を利用する人は Zsh の代りにこれにしてください
+      ;; (executable-find "f_bash") ;; Emacs + Cygwin を利用する人は Bash の代りにこれにしてください
+      (executable-find "cmdproxy")
+      (error "can't find 'shell' command in PATH!!")))
+
+;; Shell 名の設定
+(setq shell-file-name (skt:shell))
+(setenv "SHELL" shell-file-name)
+(setq explicit-shell-file-name shell-file-name)
+
+;; (cond
+;;  ((or (eq window-system 'mac) (eq window-system 'ns))
+;;   ;; Mac OS X の HFS+ ファイルフォーマットではファイル名は NFD (の様な物)で扱うため以下の設定をする必要がある
+;;   (require 'ucs-normalize)
+;;   (setq file-name-coding-system 'utf-8-hfs)
+;;   (setq locale-coding-system 'utf-8-hfs))
+;;  (or (eq system-type 'cygwin) (eq system-type 'windows-nt)
+;;   (setq file-name-coding-system 'utf-8)
+;;   (setq locale-coding-system 'utf-8)
+;;   ;; もしコマンドプロンプトを利用するなら sjis にする
+;;   ;; (setq file-name-coding-system 'sjis)
+;;   ;; (setq locale-coding-system 'sjis)
+;;   ;; 古い Cygwin だと EUC-JP にする
+;;   ;; (setq file-name-coding-system 'euc-jp)
+;;   ;; (setq locale-coding-system 'euc-jp)
+;;   )
+;;  (t
+;;   (setq file-name-coding-system 'utf-8)
+;;   (setq locale-coding-system 'utf-8)))
+(setq file-name-coding-system 'utf-8)
+(setq locale-coding-system 'utf-8)
+
+;; Emacs が保持する terminfo を利用する
+(setq system-uses-terminfo nil)
+
+;; エスケープを綺麗に表示する
+(autoload 'ansi-color-for-comint-mode-on "ansi-color" nil t)
+(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
+
+;; multi-term setting
+(add-to-list 'term-unbind-key-list '"M-x")
+
+(add-hook 'term-mode-hook
+         '(lambda ()
+            ;; C-h を term 内文字削除にする
+            (define-key term-raw-map (kbd "C-h") 'term-send-backspace)
+            ;; C-y を term 内ペーストにする
+            (define-key term-raw-map (kbd "C-y") 'term-paste)
+            ))
+
+(global-set-key (kbd "C-c n") 'multi-term-next)
+(global-set-key (kbd "C-c p") 'multi-term-prev)
