cb12523db8f9b672f1fc9f702dd3346570ccb69c
Daisuke <taisyo9170@yahoo.co.jp>
2017-09-11 11:43:37 +0900

    Adds react-native template

---
+++ .emacs.d/insert/template.js
@@ -0,0 +1,22 @@
+'use strict';
+import React, {
+  Component,
+  View,
+} from 'react-native';
+
+class %file-without-ext% extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+module.exports = %file-without-ext%;
