353c41b14d9b68ca39433232c4f16cb1f7596da8
Daisuke <taisyo9170@yahoo.co.jp>
2018-01-25 11:38:04 +0900

    uses “json-mode”  when .json file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -212,7 +212,7 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html\?\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.xml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.json\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.json\\'" . web-mode))
 
 
 ;; turn on flychecking globally
